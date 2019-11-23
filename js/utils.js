50429ca4d868e5eb423c7354198d1cc687267fa7
ywg <yuwenzhiheng95@gmail.com>
2019-11-23 15:20:06 +0800

    Site updated: 2019-11-23 15:20:05

--- js/utils.js
+++ js/utils.js
@@ -59,27 +59,19 @@ NexT.utils = {
    * One-click copy code support.
    */
   registerCopyCode: function() {
-    document.querySelectorAll('figure.highlight').forEach(e => {
-      const initButton = button => {
-        if (CONFIG.copycode.style === 'mac') {
-          button.innerHTML = '<i class="fa fa-clipboard"></i>';
-        } else {
-          button.innerText = CONFIG.translation.copy_button;
-        }
-      };
+    document.querySelectorAll('figure.highlight').forEach(element => {
       const box = document.createElement('div');
-      box.classList.add('highlight-wrap');
-      e.wrap(box);
-      e.parentNode.insertAdjacentHTML('beforeend', '<div class="copy-btn"></div>');
-      var button = e.parentNode.querySelector('.copy-btn');
+      element.wrap(box);
+      box.classList.add('highlight-container');
+      box.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard"></i></div>');
+      var button = element.parentNode.querySelector('.copy-btn');
       button.addEventListener('click', event => {
         var target = event.currentTarget;
-        var code = [...target.parentNode.querySelectorAll('.code .line')].map(element => {
-          return element.innerText;
+        var code = [...target.parentNode.querySelectorAll('.code .line')].map(line => {
+          return line.innerText;
         }).join('\n');
         var ta = document.createElement('textarea');
-        var yPosition = window.scrollY;
-        ta.style.top = yPosition + 'px'; // Prevent page scrolling
+        ta.style.top = window.scrollY + 'px'; // Prevent page scrolling
         ta.style.position = 'absolute';
         ta.style.opacity = '0';
         ta.readOnly = true;
@@ -92,7 +84,7 @@ NexT.utils = {
         ta.readOnly = false;
         var result = document.execCommand('copy');
         if (CONFIG.copycode.show_result) {
-          target.innerText = result ? CONFIG.translation.copy_success : CONFIG.translation.copy_failure;
+          target.querySelector('i').className = result ? 'fa fa-check' : 'fa fa-times';
         }
         ta.blur(); // For iOS
         target.blur();
@@ -104,36 +96,35 @@ NexT.utils = {
       });
       button.addEventListener('mouseleave', event => {
         setTimeout(() => {
-          initButton(event.target);
+          event.target.querySelector('i').className = 'fa fa-clipboard';
         }, 300);
       });
-      initButton(button);
     });
   },
 
   wrapTableWithBox: function() {
-    document.querySelectorAll('table').forEach(table => {
+    document.querySelectorAll('table').forEach(element => {
       const box = document.createElement('div');
       box.className = 'table-container';
-      table.wrap(box);
+      element.wrap(box);
     });
   },
 
   registerVideoIframe: function() {
     document.querySelectorAll('iframe').forEach(element => {
-      const SUPPORTED_PLAYERS = [
+      const supported = [
         'www.youtube.com',
         'player.vimeo.com',
         'player.youku.com',
         'player.bilibili.com',
         'www.tudou.com'
-      ];
-      const pattern = new RegExp(SUPPORTED_PLAYERS.join('|'));
-      if (!element.parentNode.matches('.video-container') && element.src.search(pattern) > 0) {
+      ].some(host => element.src.includes(host));
+      if (supported && !element.parentNode.matches('.video-container')) {
         const box = document.createElement('div');
         box.className = 'video-container';
         element.wrap(box);
-        let width = Number(element.width); let height = Number(element.height);
+        let width = Number(element.width);
+        let height = Number(element.height);
         if (width && height) {
           element.parentNode.style.paddingTop = (height / width * 100) + '%';
         }
@@ -179,20 +170,20 @@ NexT.utils = {
    */
   registerTabsTag: function() {
     // Binding `nav-tabs` & `tab-content` by real time permalink changing.
-    document.querySelectorAll('.tabs ul.nav-tabs .tab').forEach(tab => {
-      tab.addEventListener('click', event => {
+    document.querySelectorAll('.tabs ul.nav-tabs .tab').forEach(element => {
+      element.addEventListener('click', event => {
         event.preventDefault();
         var target = event.currentTarget;
         // Prevent selected tab to select again.
         if (!target.classList.contains('active')) {
           // Add & Remove active class on `nav-tabs` & `tab-content`.
-          [...target.parentNode.children].forEach(item => {
-            item.classList.remove('active');
+          [...target.parentNode.children].forEach(element => {
+            element.classList.remove('active');
           });
           target.classList.add('active');
           var tActive = document.getElementById(target.querySelector('a').getAttribute('href').replace('#', ''));
-          [...tActive.parentNode.children].forEach(item => {
-            item.classList.remove('active');
+          [...tActive.parentNode.children].forEach(element => {
+            element.classList.remove('active');
           });
           tActive.classList.add('active');
           // Trigger event
@@ -208,8 +199,8 @@ NexT.utils = {
 
   registerCanIUseTag: function() {
     // Get responsive height passed from iframe.
-    window.addEventListener('message', e => {
-      var data = e.data;
+    window.addEventListener('message', event => {
+      var data = event.data;
       if ((typeof data === 'string') && (data.indexOf('ciu_embed') > -1)) {
         var featureID = data.split(':')[1];
         var height = data.split(':')[2];
@@ -276,7 +267,7 @@ NexT.utils = {
         index = sections.indexOf(entry.target);
         return index === 0 ? 0 : index - 1;
       }
-      for (;index < entries.length; index++) {
+      for (; index < entries.length; index++) {
         if (entries[index].boundingClientRect.top <= 0) {
           entry = entries[index];
         } else {
@@ -301,7 +292,9 @@ NexT.utils = {
         rootMargin: marginTop + 'px 0px -100% 0px',
         threshold : 0
       });
-      sections.forEach(item => intersectionObserver.observe(item));
+      sections.forEach(element => {
+        element && intersectionObserver.observe(element);
+      });
     }
     createIntersectionObserver(document.documentElement.scrollHeight);
   },
@@ -309,7 +302,6 @@ NexT.utils = {
   hasMobileUA: function() {
     var ua = navigator.userAgent;
     var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
-
     return pa.test(ua);
   },
 
